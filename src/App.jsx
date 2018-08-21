8218c970dbce10698ac9738f3158d85dcb5e042f
Luis Montero <lmontero@omnicomhealthgroup.com>
2018-08-21 17:59:19 -0600

    [feature-home]-> Menu added, homepage changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import { Container, Menu, Header } from 'semantic-ui-react';
 import Home from './containers/home';
 import About from './containers/about';
 
 const App = () => (
   <div>
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
-    </header>
+    <Container>
+      <header>
+        <Header as='h1'>Geo App</Header>
+        <Menu className="main-menu">
+          <Menu.Item as={ Link } to="/">Home</Menu.Item>
+          <Menu.Item as={ Link } to="/about-us">About</Menu.Item>
+          <Menu.Item disabled>Favorite Places</Menu.Item>
+        </Menu>
+      </header>
 
-    <main>
-      <Route exact path="/" component={ Home } />
-      <Route exact path="/about-us" component={ About } />
-    </main>
+      <main>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/about-us" component={ About } />
+      </main>
+    </Container>
   </div>
+
 );
 
 export default App;
--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,49 +1,8 @@
 import React from 'react';
-import { push } from 'react-router-redux';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { Button } from 'semantic-ui-react';
-import {
-  increment,
-  incrementAsync,
-  decrement,
-  decrementAsync,
-} from '../../actions/counter';
-
-const Home = props => (
+import { Header } from 'semantic-ui-react';
+export default () => (
   <div>
-    <h1>Home</h1>
-    <p>Count: {props.count}</p>
-
-    <p>
-      <Button onClick={ props.increment } disabled={ props.isIncrementing }>Increment</Button>{' '}
-      <Button onClick={ props.incrementAsync } disabled={ props.isIncrementing }>Increment Async</Button>
-    </p>
-
-    <p>
-      <Button onClick={ props.decrement } disabled={ props.isDecrementing }>Decrementing</Button>{' '}
-      <Button onClick={ props.decrementAsync } disabled={ props.isDecrementing }>Decrement Async</Button>
-    </p>
-
-    <p><Button onClick={ () => props.changePage() }>Go to about page via redux</Button></p>
+    <Header as='h1'>Countries</Header>
+    <p>Hello Medium!</p>
   </div>
 );
-
-const mapStateToProps = state => ({
-  count: state.counter.count,
-  isIncrementing: state.counter.isIncrementing,
-  isDecrementing: state.counter.isDecrementing,
-});
-
-const mapDispatchToProps = dispatch => bindActionCreators({
-  increment,
-  incrementAsync,
-  decrement,
-  decrementAsync,
-  changePage: () => push('/about-us'),
-}, dispatch);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
