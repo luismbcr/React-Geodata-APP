74c32b921c14a7dd9ec0561f9502977db0716387
Luis Montero <lmontero@omnicomhealthgroup.com>
2018-09-06 18:39:53 -0600

    mobile improvement

--- src/components/country/index.jsx
+++ src/components/country/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Card, Image } from 'semantic-ui-react';
 
 export default (props) => (
-  <Card.Group as={ Link } to={ `country/${props.id}` } className="country" >
+  <Card.Group as={ Link } to={ `country/${props.id}` } className="country" stackable >
     <Card>
       <Card.Content>
         <Image floated='right' size='mini' src={ props.flag } />
